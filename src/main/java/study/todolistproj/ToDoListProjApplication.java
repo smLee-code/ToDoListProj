171daad954c633d8a8d5850f046950b6867ed4f0
Sang-Myeong Lee <teamjuji5@naver.com>
2024-12-25 02:28:12 +0900

    feat: ToDoList created

---
+++ src/main/java/study/todolistproj/ToDoListProjApplication.java
@@ -0,0 +1,13 @@
+package study.todolistproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/todolistproj/ToDoListProjApplicationTests.java
@@ -0,0 +1,13 @@
+package study.todolistproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
